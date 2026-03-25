21dbf79a6642a6a2ba42a5ee9b670cc56a7c83bb
xArsenic <76486130+xYeshu@users.noreply.github.com>
2026-03-26 05:09:41 +0530

    Update button text to remove reference to Yeshu

--- client/src/components/Pricing.jsx
+++ client/src/components/Pricing.jsx
@@ -44,7 +44,7 @@ const Example = () => {
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
       >
-        Just Kidding, Its FREE Forever for Yeshu's friends :)
+        Just Kidding, Its FREE :)
       </button>
     </div>
   );
